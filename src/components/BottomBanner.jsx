1bd7f6c7f56c92fc88be61ab1bd3d97df65e1049
Amit Verma <vamit20000@gmail.com>
2025-05-20 00:39:59 +0530

    bestseller section done and partial bottom banner done

---
+++ src/components/BottomBanner.jsx
@@ -0,0 +1,19 @@
+import {assets} from "../assets/assets.js";
+const BottomBanner = () => {
+  return (
+    <div className="relative mt-24">
+      <img
+        src={assets.bottom_banner_image}
+        alt="banner"
+        className="w-full hidden md:block"
+      />
+      <img
+        src={assets.bottom_banner_image_sm}
+        alt="banner"
+        className="w-full  md:hidden"
+      />
+    </div>
+  );
+};
+
+export default BottomBanner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
+import BestSeller from "../components/BestSeller"
+import Categories from "../components/Categories"
 import MainBanner from "../components/MainBanner"
+
+import BottomBanner from "../components/BottomBanner"
 
 
 const Home = () => {
   return (
     <div className="mt-10">
         <MainBanner/>
+        <Categories/>
+        <BestSeller/>
+        <BottomBanner/>
     </div>
   )
 }
